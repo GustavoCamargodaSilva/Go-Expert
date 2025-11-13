8a8cc36332528bd01d1d27d3b21eed2494f23b27
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2025-11-13 08:10:52 -0300

    ponteiros

--- Aula 15 - Ponteiros/main.go
+++ Aula 15 - Ponteiros/main.go
@@ -1,5 +1,21 @@
 package main
 
+type Cliente struct {
+	nome  string
+	idade int
+}
+
+func soma(a *int) {
+	*a = 100
+}
+
 func main() {
+
+	//Memória  -> Endereço -> Valor ->
+	// a := 10
+	// fmt.Println(a)
+
+	// soma(&a)
+	// fmt.Println(a)
 
 }
