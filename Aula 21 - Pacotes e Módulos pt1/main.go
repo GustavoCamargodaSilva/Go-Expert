d46b7a364a3d19781da494658c99f8f29a8c95f2
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2025-11-23 18:13:51 -0300

    importantes conceitos sobre modulos

--- Aula 21 - Pacotes e Módulos pt1/main.go
+++ Aula 21 - Pacotes e Módulos pt1/main.go
@@ -6,7 +6,13 @@ import (
 	"github.com/GustavoCamargodaSilva/api-gateway-golang/matematica" // Atençao a pasta na qual vai ser colocado o go.mod, precisa respeitar a hierarquia de pastas de import.
 )
 
+// func com letra maiuscula é exportado
+// func com letra minuscula nao é exportado
+
 func main() {
 	s := matematica.Soma(10, 20)
 	fmt.Println("Resultado: ", s)
+
+	carro := matematica.Carro{Modelo: "Fiat"}
+	fmt.Println(carro.Modelo)
 }
--- Aula 21 - Pacotes e Módulos pt1/matematica/matematica.go
+++ Aula 21 - Pacotes e Módulos pt1/matematica/matematica.go
@@ -3,3 +3,9 @@ package matematica
 func Soma[T int | float64](a, b T) T {
 	return a + b
 }
+
+// var a int = 10
+
+type Carro struct { //Se a struct for com letra maiúscula mas
+	Modelo string // -> o atributo for com letra minuscula, a struct fica visivel mas o Modelo nao.
+}
