dea2b67105759b1f8a61c4850a69d61a4d78ce83
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2025-11-23 23:59:34 -0300

    fim dos fundations

---
+++ Aula 25 - Compilando Projetos/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	//para compitar -> go build "nome do arquivo"
+
+	// GOOS=windows go build main.go -> ele builda exclusivamente para windows
+	//GOOS=linux -> podemos fazer o build com base no sistema especifico
+
+}
