694acf78822d8a0ccd2db1bf691df0d663e075f6
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2025-11-26 20:49:05 -0300

    exercicio 01

---
+++ Foundations Pratice/02 - Declaracao e Titulo/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+//Exercício 1: Declaração de Constantes
+//Crie um programa que declare uma constante para o nome de um produto e exiba uma mensagem usando essa constante.
+// A constante deve estar no escopo global do pacote.
+
+const produto = "Celular"
+
+func main() {
+
+	fmt.Println("Produto:", produto)
+
+}
